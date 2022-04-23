ed5f2d948ed37fad5154eebd53279ba07abcc3aa
Alex <alexandru_braic@yahoo.com>
2022-04-23 22:36:36 +0100

    initial commit for this version of the API

---
+++ Data/CommentsServiceContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CommentsService.Models;
+
+namespace CommentsService.Data
+{
+    public class CommentsServiceContext : DbContext
+    {
+        public CommentsServiceContext (DbContextOptions<CommentsServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CommentsService.Models.Comment> Comment { get; set; }
+    }
+}
